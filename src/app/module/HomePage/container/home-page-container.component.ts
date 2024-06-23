b51d45a6cf7c1d22a458d83b59416e5309579d0a
TOLOES <tomdehame@orange.fr>
2024-06-23 12:21:28 +0200

    feature: Add unit tests

--- src/app/module/HomePage/container/home-page-container.component.ts
+++ src/app/module/HomePage/container/home-page-container.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {HomePageComponent} from "../components/home-page.component";
-import {HeaderComponent} from "../../../shared/components/header/header.component";
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from '../components/home-page.component';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-home-page-container',
   standalone: true,
   imports: [CommonModule, HomePageComponent, HeaderComponent],
   templateUrl: './home-page-container.component.html',
-  styleUrl: './home-page-container.component.scss',
 })
 export class HomePageContainerComponent {}
